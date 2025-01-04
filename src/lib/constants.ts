a6a430332cb06a6420f59559eeebafeebabf62e1
antonio-abrantes <antonio-abrantes@hotmail.com.br>
2025-01-03 22:20:26 -0300

    Updates

--- src/lib/constants.ts
+++
@@ -1,7 +0,0 @@
-export const GITHUB_API_BASE_URL = 'https://api.github.com';
-export const SOCIAL_LINKS = {
-  github: 'https://github.com/antonio-abrantes',
-  linkedin: 'https://linkedin.com/in/antonio-abrantes',
-  twitter: 'https://x.com/Toin_Abrantes',
-  instagram: 'https://www.instagram.com/antonio.abrantes',
-} as const;
