b052b9e1aec14baf86f17ed6c4b0673af92c948a
antonio-abrantes <antonio-abrantes@hotmail.com.br>
2025-01-12 10:22:26 -0300

    Updates

--- src/components/Footer.tsx
+++
@@ -1,9 +0,0 @@
-export function Footer() {
-  return (
-    <footer className="h-[40px] w-full bg-transparent flex items-center justify-center">
-      <div className="text-white text-[11px]">
-        By Antônio Abrantes © {new Date().getFullYear()}
-      </div>
-    </footer>
-  );
-}
